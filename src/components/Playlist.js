ab7fd0fdfc25a4860ad7b288c76aafb00c9d5b7d
eaball35 <eaball35@gmail.com>
2019-12-08 17:13:10 -0800

    favorite and top list

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -3,55 +3,74 @@ import PropTypes from 'prop-types'
 import './styles/Playlist.css';
 
 import Track from './Track';
+class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trackElements: props.tracks.map((track, i) => {
+        // We use "spread syntax" here to pass in all the properties of the variable 'track' as props. Go look it up!
+        return (
+          <Track
+            key={track.id}
+            {...track}
+            trackToTopCallback={this.trackToTop}
+          />
+        );
+      }),
 
-const calculatePlayTime = (tracks) => {
-  let minutes = 0;
-  let seconds = 0;
-  tracks.forEach((track) => {
-    const times = track.playtime.split(':');
-    minutes += parseInt(times[0]);
-    seconds += parseInt(times[1]);
-  });
+    };
+  }
 
-  minutes += Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
+  calculatePlayTime = (tracks) => {
+    let minutes = 0;
+    let seconds = 0;
+    tracks.forEach((track) => {
+      const times = track.playtime.split(':');
+      minutes += parseInt(times[0]);
+      seconds += parseInt(times[1]);
+    });
+  
+    minutes += Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+  
+    seconds %= 60;
+    minutes %= 60;
+  
+    seconds = ("" + seconds).padStart(2, "0");
+    minutes = ("" + minutes).padStart(2, "0");
+  
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  
+  trackToTop = (track) => {
+    let orderTracks = [track]
+    
+    this.state.trackElements.forEach(function(song) {
+      if (song.props.id !== track.props.id) {
+        orderTracks.push(song);
+      }
+    });
 
-  seconds %= 60;
-  minutes %= 60;
+    console.log(orderTracks)
+    
+    this.setState({ trackElements: orderTracks});
+  }
+  
 
-  seconds = ("" + seconds).padStart(2, "0");
-  minutes = ("" + minutes).padStart(2, "0");
-
-  return `${hours}:${minutes}:${seconds}`;
-}
-
-const Playlist = (props) => {
-  const tracks = props.tracks;
-  const trackCount = tracks.length;
-  const playtime = calculatePlayTime(tracks);
-  const trackElements = tracks.map((track, i) => {
-    // We use "spread syntax" here to pass in all the properties of 
-    // the variable 'track' as props. Go look it up!
+  render () {
     return (
-      <Track
-        key={track.id}
-        {...track}
-      />
+      <div className="playlist">
+        <h2>{this.props.side} Playlist</h2>
+        <p>
+          {this.props.trackCount} tracks - {this.props.playtime}
+        </p>
+        <ul className="playlist--track-list">
+          {this.state.trackElements}
+        </ul>
+      </div>
     );
-  });
-
-  return (
-    <div className="playlist">
-      <h2>{props.side} Playlist</h2>
-      <p>
-        {trackCount} tracks - {playtime}
-      </p>
-      <ul className="playlist--track-list">
-        {trackElements}
-      </ul>
-    </div>
-  );
-}
+  };
+};
 
 Playlist.propTypes = {
   tracks: PropTypes.array,
